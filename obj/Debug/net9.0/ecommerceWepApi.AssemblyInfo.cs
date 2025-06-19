7f4baf11c8d58285062454ea4778c1b91438842b
Owoyemi Olusola <owoyemisholly@gmail.com>
2025-06-19 15:00:11 +0100

    ensure I can save and retrieve from data base

--- obj/Debug/net9.0/ecommerceWepApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ecommerceWepApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerceWepApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89f6e2b8aa393b1dcc872b42492db00634f40aea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69e3a17d0150cb4fc1067047c415c42c195ffcce")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerceWepApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerceWepApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
